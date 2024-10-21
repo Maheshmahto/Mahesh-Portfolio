5010169512d073d80930c217783b2dc2d0acda5f
mahesh mahto <maheshmahto1806@gmail.com>
2024-10-21 19:51:34 +0530

    path change

--- src/Home/BackGroud.jsx
+++ src/Home/BackGroud.jsx
@@ -3,8 +3,8 @@ import './Home.css'
 const BackGroud = () => {
   return (
     <div className='home-name' >
-     <img className='blackhole' src="public/black-holes-removebg-preview.png" alt="" />
-      <img className='earth-img' src="public/earthPlanet.png" alt="" />
+     <img className='blackhole' src="public/black-holes-removebg-preview.png"  alt="" />
+      <img className='earth-img' src={"public/earthPlanet.png" }alt="" />
          <h1 className='myname'>Mahesh </h1>
 
     </div>
